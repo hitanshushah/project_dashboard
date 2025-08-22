8c14c462dc894f7fff7612895efe025420c4ec3c
hitanshushah <hitanshushah10.8@gmail.com>
2025-08-21 20:19:59 -0400

    fix: prod

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>Project Board</title>
-    @vite('resources/js/app.js')
+    @vite('resources/js/app.ts')
     @inertiaHead
 </head>
 <body>
